d8548b0361f12805f453bbd35db3579f08b1ec68
Aniket <Aniket.Gunjal@mindtree.com>
2023-06-26 11:37:39 +0530

    Initial Commit

--- src/main/java/com/Hello/HelloApplication.java
+++ src/main/java/com/Hello/HelloApplication.java
@@ -2,24 +2,36 @@ package com.Hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+
 public class HelloApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloApplication.class, args);
+		
 	}
 	
-	@RestController
-	public class helloWorld{
-		@RequestMapping(value= "/Hello")
+	
+	@RestController 
+	class helloWorld{
+		@RequestMapping(value= "/")
 		
-	public String hello() {
-			return "Hello World";
+		//@GetMapping("/")    
+		public String hello() {         
+		return "<h1><b>Service is up and running</b></h1>"; 
 		}
 		
+	//public String hello() {
+			//return "Hello World";
+	
+		//}
+		
+	
 	}
 
 }
